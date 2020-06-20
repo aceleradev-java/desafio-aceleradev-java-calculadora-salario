42072000ae752c6ba7a93f62d05245977aa0d03c
Adriano Avelino <adriano-st777@hotmail.com>
2020-06-19 22:15:27 -0300

    implementa os métodos para realizar os descontos

--- src/main/java/br/com/codenation/calculadora/CalculadoraSalario.java
+++ src/main/java/br/com/codenation/calculadora/CalculadoraSalario.java
@@ -1,20 +1,46 @@
 package br.com.codenation.calculadora;
 
-
 public class CalculadoraSalario {
 
-	public long calcularSalarioLiquido(double salarioBase) {
-		//Use o Math.round apenas no final do método para arredondar o valor final.
-		//Documentação do método: https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html#round-double-
-		return Math.round(0.0);
-	}
-	
-	
-	//Exemplo de método que pode ser criado para separar melhor as responsábilidades de seu algorítmo
-	private double calcularInss(double salarioBase) {
-		return 0.0;
-	}
+    public long calcularSalarioLiquido(double salarioBase) {
+        //Use o Math.round apenas no final do método para arredondar o valor final.
+        //Documentação do método: https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html#round-double-
+        
+        if (salarioBase <= 1039) {
+            return 0;
+        }
+        double descontoINSS = calcularInss(salarioBase);
+        double descontoIRRF = calcularIRRF(salarioBase - descontoINSS);
+        double salarioLiquido = salarioBase - descontoINSS - descontoIRRF;
+        
+        return Math.round(salarioLiquido);
+    }
 
+    //Exemplo de método que pode ser criado para separar melhor as responsábilidades de seu algorítmo
+    private double calcularInss(double salarioBase) {
+        double descontoINSS = 0.0;
+        
+        if (salarioBase <= 1500) {
+            descontoINSS = salarioBase * 0.08; //8%
+        } 
+        else if (salarioBase <= 4000) {
+            descontoINSS = salarioBase * 0.09; // 9%
+        } 
+        else {
+            descontoINSS = salarioBase * 0.11; // 11
+        }
+        return descontoINSS;
+    }
+
+    private double calcularIRRF(double salarioComDescontoINSS) {
+        double desconto = 0.0;
+        if (salarioComDescontoINSS > 3000 && salarioComDescontoINSS <= 6000) {
+            desconto = salarioComDescontoINSS * 0.075;
+        } else if(salarioComDescontoINSS > 6000){
+            desconto = salarioComDescontoINSS * 0.15;
+        }
+        return desconto;
+    }
 }
 /*Dúvidas ou Problemas?
 Manda e-mail para o meajuda@codenation.dev que iremos te ajudar! 
--- src/test/java/br/com/codenation/calculadora/CalculadoraSalarioTest.java
+++ src/test/java/br/com/codenation/calculadora/CalculadoraSalarioTest.java
@@ -1,14 +1,52 @@
 package br.com.codenation.calculadora;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
 public class CalculadoraSalarioTest {
 
-	@Test
-	public void salarioLiquidoIsNotNull() {
-		assertNotNull(new CalculadoraSalario().calcularSalarioLiquido(1000.0));
-	}
+    @Test
+    public void salarioLiquidoIsNotNull() {
+        assertNotNull(new CalculadoraSalario().calcularSalarioLiquido(1000.0));
+    }
 
+    @Test
+    public void deveCalcularDescontoDeINSSDeOitoPorcento() {
+        double salarioBase = 100.0;
+        double salarioLiquido = 0;
+        assertEquals(salarioLiquido ,new CalculadoraSalario().calcularSalarioLiquido(salarioBase), 0);
+    }
+
+    @Test
+    public void deveCalcularDescontoDeINSSDeNovePorcento() {
+        double salarioBase = 1500.01;
+        double salarioLiquido = 1365.00;
+        assertEquals(salarioLiquido ,new CalculadoraSalario().calcularSalarioLiquido(salarioBase), 0);
+    }
+
+    @Test
+    public void deveCalcularDescontoDeINSSDeNovePorcentoMaisDescontoIRRFDeSeteEMeioPorcento() {
+        double salarioBase = 4000;
+        double salarioLiquido = 3367.0;
+        assertEquals(salarioLiquido ,new CalculadoraSalario().calcularSalarioLiquido(salarioBase), 0);
+        salarioBase = 3500.0;
+        salarioLiquido = 2946.0;
+        assertEquals(salarioLiquido ,new CalculadoraSalario().calcularSalarioLiquido(salarioBase), 0);
+    }
+
+    @Test
+    public void deveCalcularDescontoDeINSSDeOnzePorcento() {
+        double salarioBase = 5000;
+        double salarioLiquido = 4116.0;
+        assertEquals(salarioLiquido ,new CalculadoraSalario().calcularSalarioLiquido(salarioBase), 0);
+    }
+
+    @Test
+    public void deveCalcularDescontoDeINSSDeOnzePorcentoMaisDescontoDeIRRFDeQuinzePorcento() {
+        double salarioBase = 10000;
+        double salarioLiquido = 7565;
+        assertEquals(salarioLiquido ,new CalculadoraSalario().calcularSalarioLiquido(salarioBase), 0);
+    }
 }
